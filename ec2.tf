686a36ac87b9402d4626c48bb0815626682d00b3
Hamza Abu Maizer <Hamzehabumaizer1@gmail.com>
2022-04-05 21:47:31 +0300

    new

--- ec2.tf
+++ ec2.tf
@@ -12,14 +12,4 @@ resource "aws_eip_association" "eip_assoc" {
   instance_id   = 	aws_instance.web.id
   allocation_id =   aws_eip.lb.id
 }
-
-resource "aws_security_group" "allow_tls" {
-  name        = "terraform-security-group"
-
-  ingress {
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["${aws_eip.lb.public_ip}"/32]
-  }
   }
